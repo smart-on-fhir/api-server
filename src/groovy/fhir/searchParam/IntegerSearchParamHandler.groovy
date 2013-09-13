cd67f2f2fc320134a80baa5292f6852759720759
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2013-09-12 22:27:25 -0700

    Added missing search param handlers

---
+++ src/groovy/fhir/searchParam/IntegerSearchParamHandler.groovy
@@ -0,0 +1,13 @@
+package fhir.searchParam
+
+import org.hl7.fhir.instance.model.Resource
+import org.hl7.fhir.instance.model.Conformance.SearchParamType
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+
+import com.mongodb.BasicDBObject;
+
+// Need clarification about how this is different from other
+// numerical types (double, say).
+public class IntegerSearchParamHandler extends StringSearchParamHandler {}
+
---
+++ src/groovy/fhir/searchParam/ReferenceSearchParamHandler.groovy
@@ -0,0 +1,37 @@
+package fhir.searchParam
+
+import org.hl7.fhir.instance.model.Conformance.SearchParamType
+import org.w3c.dom.Node
+
+import com.mongodb.BasicDBObject
+public class ReferenceSearchParamHandler extends SearchParamHandler {
+
+	@Override
+	protected void processMatchingXpaths(List<Node> nodes, List<SearchParamValue> index) {
+		nodes.each {
+			index.add(value(queryString('./f:reference/@value', it)));
+		}
+	}
+
+	@Override
+	protected String paramXpath() {
+		return "//"+this.xpath;
+	}
+
+	@Override
+	BasicDBObject searchClause(Map searchedFor){
+		// FHIR spec describes a slight difference between
+		// no modifier and ":text" on a code --
+		// but we're treating them the same here
+		if (searchedFor.modifier == null){
+			return [(fieldName):searchedFor.value]
+		}
+
+		if (searchedFor.modifier == "any"){
+			return [(fieldName):[$regex: '/'+searchedFor.value+'$']]
+		}
+
+		throw new RuntimeException("Unknown modifier: " + searchedFor)
+	}
+
+}
