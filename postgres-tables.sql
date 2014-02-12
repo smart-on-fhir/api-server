dd8c7f5b414bce2aa5f7fde9cd9126719c56c075
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2014-02-12 15:58:02 -0500

    Don't hard-code 'fhir' user in db create scripts

--- postgres-tables.sql
+++ postgres-tables.sql
@@ -1,32 +1,8 @@
---
--- PostgreSQL database dump
---
-
-SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
---
--- Name: hibernate_sequence; Type: SEQUENCE; Schema: public; Owner: fhir
---
 
 CREATE SEQUENCE hibernate_sequence
     START WITH 1
@@ -36,15 +12,10 @@ CREATE SEQUENCE hibernate_sequence
     CACHE 1;
 
 
-ALTER TABLE public.hibernate_sequence OWNER TO fhir;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
---
--- Name: resource_compartment; Type: TABLE; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE TABLE resource_compartment (
     fhir_type character varying(255) NOT NULL,
@@ -53,11 +24,6 @@ CREATE TABLE resource_compartment (
 );
 
 
-ALTER TABLE public.resource_compartment OWNER TO fhir;
-
---
--- Name: seq_resource_index_term; Type: SEQUENCE; Schema: public; Owner: fhir
---
 
 CREATE SEQUENCE seq_resource_index_term
     START WITH 1
@@ -67,11 +33,6 @@ CREATE SEQUENCE seq_resource_index_term
     CACHE 1;
 
 
-ALTER TABLE public.seq_resource_index_term OWNER TO fhir;
-
---
--- Name: resource_index_term; Type: TABLE; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE TABLE resource_index_term (
     id bigint DEFAULT nextval('seq_resource_index_term'::regclass) NOT NULL,
@@ -96,11 +57,6 @@ CREATE TABLE resource_index_term (
 );
 
 
-ALTER TABLE public.resource_index_term OWNER TO fhir;
-
---
--- Name: resource_version; Type: TABLE; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE TABLE resource_version (
     version_id bigint NOT NULL,
@@ -112,11 +68,6 @@ CREATE TABLE resource_version (
 );
 
 
-ALTER TABLE public.resource_version OWNER TO fhir;
-
---
--- Name: seq_resource_compartment; Type: SEQUENCE; Schema: public; Owner: fhir
---
 
 CREATE SEQUENCE seq_resource_compartment
     START WITH 1
@@ -126,11 +77,6 @@ CREATE SEQUENCE seq_resource_compartment
     CACHE 1;
 
 
-ALTER TABLE public.seq_resource_compartment OWNER TO fhir;
-
---
--- Name: seq_resource_version; Type: SEQUENCE; Schema: public; Owner: fhir
---
 
 CREATE SEQUENCE seq_resource_version
     START WITH 1
@@ -140,134 +86,76 @@ CREATE SEQUENCE seq_resource_version
     CACHE 1;
 
 
-ALTER TABLE public.seq_resource_version OWNER TO fhir;
-
---
--- Name: hibernate_sequence; Type: SEQUENCE SET; Schema: public; Owner: fhir
---
 
 SELECT pg_catalog.setval('hibernate_sequence', 1219157, true);
 
 
---
--- Name: seq_resource_compartment; Type: SEQUENCE SET; Schema: public; Owner: fhir
---
 
 SELECT pg_catalog.setval('seq_resource_compartment', 13448, true);
 
 
---
--- Name: seq_resource_index_term; Type: SEQUENCE SET; Schema: public; Owner: fhir
---
 
 SELECT pg_catalog.setval('seq_resource_index_term', 412599, true);
 
 
---
--- Name: seq_resource_version; Type: SEQUENCE SET; Schema: public; Owner: fhir
---
 
 SELECT pg_catalog.setval('seq_resource_version', 107747, true);
 
 
---
--- Name: resource_compartment_pkey; Type: CONSTRAINT; Schema: public; Owner: fhir; Tablespace: 
---
 
 ALTER TABLE ONLY resource_compartment
     ADD CONSTRAINT resource_compartment_pkey PRIMARY KEY (fhir_type, fhir_id);
 
 
---
--- Name: resource_index_term_pkey; Type: CONSTRAINT; Schema: public; Owner: fhir; Tablespace: 
---
 
 ALTER TABLE ONLY resource_index_term
     ADD CONSTRAINT resource_index_term_pkey PRIMARY KEY (id);
 
 
---
--- Name: resource_version_pkey; Type: CONSTRAINT; Schema: public; Owner: fhir; Tablespace: 
---
 
 ALTER TABLE ONLY resource_version
     ADD CONSTRAINT resource_version_pkey PRIMARY KEY (version_id);
 
 
---
--- Name: logical_id; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX logical_id ON resource_index_term USING btree (fhir_id, fhir_type);
 
 
---
--- Name: logical_id_searchparam_value; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX logical_id_searchparam_value ON resource_index_term USING btree (fhir_type, search_param);
 
 
---
--- Name: ref_logical_id; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX ref_logical_id ON resource_index_term USING btree (reference_id, reference_type);
 
 
---
--- Name: search_param_reference; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX search_param_reference ON resource_index_term USING btree (search_param, reference_type, reference_id);
 
 
---
--- Name: search_param_string; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX search_param_string ON resource_index_term USING btree (search_param, string_value);
 
 
---
--- Name: search_param_token; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX search_param_token ON resource_index_term USING btree (fhir_type, search_param, token_code, token_namespace);
 
 
---
--- Name: term_version_id; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX term_version_id ON resource_index_term USING btree (version_id);
 
 
---
--- Name: version_logical_id; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX version_logical_id ON resource_version USING btree (fhir_type, fhir_id);
 
 
---
--- Name: version_version_id; Type: INDEX; Schema: public; Owner: fhir; Tablespace: 
---
 
 CREATE INDEX version_version_id ON resource_version USING btree (version_id);
 
 
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
 
