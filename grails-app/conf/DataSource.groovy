b7b4597f04dcb991ffb98d6660dadb1b00490136
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2014-07-15 12:07:01 -0400

    Periodically evict dead DB connections

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -10,6 +10,25 @@ dataSource {
     username = "fhir"
     password = "fhir"
     dbCreate = "update"
+	
+	properties {
+		maxActive = 50
+		maxIdle = 25
+		minIdle = 1
+		initialSize = 1
+   
+		numTestsPerEvictionRun = 3
+		maxWait = 10000
+   
+		testOnBorrow = true
+		testWhileIdle = true
+		testOnReturn = true
+   
+		validationQuery = "select 1"
+   
+		minEvictableIdleTimeMillis = 1000 * 60 * 5
+		timeBetweenEvictionRunsMillis = 1000 * 60 * 5
+	 }
 }
 
 
